b81f25912c076d593e6fbc876cb98fed8669e0a1
Annegret Pohle <apohle@ethz.ch>
2022-12-01 18:07:14 -0900

    fixed 1D comparison

--- examples/single_variables.jl
+++ examples/single_variables.jl
@@ -1,25 +1,28 @@
-using SliceSamplingProject, PyPlot, Distributions, StatsBase, LinearAlgebra, Infiltrator
-close()
+using SliceSamplingProject, PyPlot, Distributions, StatsBase, LinearAlgebra
+close("all") # close all open figures
 
-n = 120
+n = 150
 
 # Gaussian
 σ1   = 2.0
 #f(x) = 1/(σ1*sqrt(2π)) * exp(-x^2/(2σ1^2))
 
+# exponential
+# f(x) = pdf(Exponential(1.0), x)
+# xstart,xend = (0.,10.)
+# k = 1.
+
 # bimodal
 σ2 = 12.0
 f(x) = 1/(σ1*sqrt(2π)) * exp(-x^2/(2σ1^2)) + 1/(σ2*sqrt(2π)) * exp(-(x-60)^2/(2σ2^2))
 xstart,xend = (-15., 100.)
+k = 2.
+
 xs = range(xstart,xend,length=n)
 dx = xs[2]-xs[1]
-xedges = range(xstart-0.5dx, xend+0.5dx, length=n+1)
-k  = sum(f.(xs))*dx
-true_pdf = f.(xs)./k
+xedges = range(xstart, xend + dx, length=n+1)
+true_pdf = f.(xs) ./k
 
-
-# exponential
-# f(x) = pdf(Exponential(0.1), x)
 
 # f(x) = pdf(Uniform(0,1), x)
 
@@ -28,64 +31,49 @@ true_pdf = f.(xs)./k
 
 # run the slice sampling
 x0  = 0.5
-ws = 0.1:0.2:10.0
-its = floor.(Int,10 .^(2:0.2:6))
-methods = ["step_out", "doubling"]
-# err = zeros(length(its),length(methods))
-err = zeros(length(ws),length(methods))
-hs  = zeros(n,length(methods),length(ws))
-true_pdf  = zeros(n,length(methods),length(ws))
-for (m, method) in enumerate(methods)
-    for (i,w) in enumerate(ws)
-        # method = "step_out"; iters = 1000000
-        x = single_var(f,x0;iters=10^4,w,method)
-        figure(1)
-        h_hist = hist(x,bins=n,color="grey",alpha=0.4,density=true);
-        # h = fit(Histogram, x, xedges)
-        # h = normalize(h, mode=:density)
-        # err[i,m] = norm(h.weights - f.(xs)./k)
-        err[i,m] = norm(h_hist[1] - f.(xs)./k,1)
-        # hs[:,m,i] = h.weights
-        hs[:,m,i] = h_hist[2][2:end]
+
+ws = 1.0:1.:12.0
+its = floor.(Int,10 .^[4,5,6,7])
+err = Dict{String, Any}("step_out" => zeros(length(ws),length(its)),
+                        "doubling" => zeros(length(ws),length(its)))
+hs  = Dict{String, Any}("step_out" => zeros(length(ws),length(its),n),
+                        "doubling" => zeros(length(ws),length(its),n))
+
+# Slice sampling
+for (m, method) in enumerate(keys(err))
+    for (j,iters) in enumerate(its)
+        println("Slice sampling for $iters iterations..")
+        for (i,w) in enumerate(ws)
+            x = single_var(f,x0;iters,w,method)
+            h = fit(Histogram, x, xedges)
+            h_density = h.weights ./ (sum(h.weights)*dx)            # specific for exponential function!
+            err[method][i,j] = norm(h_density - true_pdf,1)
+            hs[method][i,j,:] = h_density
+        end
     end
 end
-figure(2)
-plot(ws,err[:,1],label="step out")
-plot(ws,err[:,2],label="doubling")
-title("$n bins")
-legend()
-xscale("log")
-yscale("log")
-
-# for b = 80:40:140
-#         figure(1); h2 = hist(x,bins=b,density=true)
-#         figure(2); plot(h2[2][2:end],h2[1],label="$b")
-# end
-# legend()
-
-# x_p = sample(xs,Weights(f.(xs)./k),n; ordered=true)
-
 
 # Metropolis
-ws  = 1.0:5:30
-err = zeros(length(ws))
-hs  = zeros(n,length(ws))
+MHs  = ws
+err["Metropolis"] = zeros(length(ws),length(its))
+hs["Metropolis"]  = zeros(length(ws),length(its),n)
 acc_ratios = zeros(length(ws))
-#     for (i,iters) in enumerate(its)
-iters = 10^4
-for (i,MH_width) in enumerate(ws)
-    x, acc = metropolis(x0, x->log(f(x)) ; nsims=iters, MH_width)
-    figure(1)
-    h_hist = hist(x,bins=n,color="grey",alpha=0.4,density=true);
-    err[i] = norm(h_hist[1] - f.(xs)./k,1)
-    hs[:,i] = h_hist[2][2:end]
-    acc_ratios[i] = acc
+
+for (j,iters) in enumerate(its)
+    println("Metropolis sampling for $iters iterations..")
+    for (i,MH_width) in enumerate(MHs)
+        x, acc = metropolis(x0, x->log(f(x)) ; nsims=iters, MH_width)
+        h = fit(Histogram, x, xedges)
+        h_density = h.weights ./ (sum(h.weights)*dx)
+        err["Metropolis"][i,j] = norm(h_density - true_pdf,1)
+        hs["Metropolis"][i,j,:] = h_density
+        acc_ratios[i] = acc
+    end
 end
 
-figure(13)
-plot(ws,err)
-title("Metropolis, err")
-yscale("log")
-figure(14)
-plot(ws,acc_ratios)
-title("Metropolis, acc_ratios")
+figure(1)
+for (m,method) in enumerate(keys(err))
+    subplot(1,3,m)
+    pcolor(err[method][2:end,:]); title(method); colorbar(); clim([0, maximum([maximum(err[m]) for m in keys(err)]) ])
+    xlabel("# iterations"); ylabel("w")
+end
