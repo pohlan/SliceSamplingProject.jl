c6b3efa5e5b523069d8360b692e84447141086fb
Annegret Pohle <apohle@ethz.ch>
2022-12-04 21:21:57 -0900

    settled on some preliminary plots

--- examples/single_variables.jl
+++ examples/single_variables.jl
@@ -1,18 +1,12 @@
-using SliceSamplingProject, PyPlot, Distributions, StatsBase, LinearAlgebra
+using SliceSamplingProject, PyPlot, Distributions, StatsBase, LinearAlgebra, LaTeXStrings
 close("all") # close all open figures
+rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams")
+rcParams["font.size"] = 30
 
-n = 150
+n = 200
 
-# Gaussian
-σ1   = 2.0
-#f(x) = 1/(σ1*sqrt(2π)) * exp(-x^2/(2σ1^2))
-
-# exponential
-# f(x) = pdf(Exponential(1.0), x)
-# xstart,xend = (0.,10.)
-# k = 1.
-
-# bimodal
+# bimodal distribution
+σ1 = 2.0
 σ2 = 12.0
 f(x) = 1/(σ1*sqrt(2π)) * exp(-x^2/(2σ1^2)) + 1/(σ2*sqrt(2π)) * exp(-(x-60)^2/(2σ2^2))
 xstart,xend = (-15., 100.)
@@ -22,18 +16,15 @@ xs = range(xstart,xend,length=n)
 dx = xs[2]-xs[1]
 xedges = range(xstart, xend + dx, length=n+1)
 true_pdf = f.(xs) ./k
-
-
-# f(x) = pdf(Uniform(0,1), x)
-
-# Beta
-# f(x) = pdf(Beta(1.0,10.0), x)
+figure(11,figsize=(12,7))
+plot(xs,true_pdf,lw=2,"black")
+xlabel("x"); ylabel("f(x)")
+savefig("true_pdf.jpeg")
 
 # run the slice sampling
 x0  = 0.5
-
-ws = 1.0:1.:12.0
-its = floor.(Int,10 .^[4,5,6,7])
+ws = 1.0:1.:45
+its = floor.(Int,10 .^[4,4.5,5,5.5,6])
 err = Dict{String, Any}("step_out" => zeros(length(ws),length(its)),
                         "doubling" => zeros(length(ws),length(its)))
 hs  = Dict{String, Any}("step_out" => zeros(length(ws),length(its),n),
@@ -71,9 +62,38 @@ for (j,iters) in enumerate(its)
     end
 end
 
-figure(1)
+# compare error of all
+figure(1, figsize=(18,12))
 for (m,method) in enumerate(keys(err))
     subplot(1,3,m)
-    pcolor(err[method][2:end,:]); title(method); colorbar(); clim([0, maximum([maximum(err[m]) for m in keys(err)]) ])
-    xlabel("# iterations"); ylabel("w")
+    pcolor(log.(10,its),Array(ws),err[method][1:end,:]); title(method)
+    xlabel(L"$log_{10}$ (iterations)")
+    if m == 1
+        ylabel(L"w")
+    end
+    if m == 3
+        c = colorbar(); clim([0, maximum([maximum(err[m]) for m in keys(err)]) ])
+        c.set_label("error")
+    end
 end
+savefig("error_comparison.jpeg")
+
+# some exapmles
+figure(2, figsize=(16,10))
+subplot(1,2,1)
+for (m,method) in enumerate(keys(err))
+    plot(xs,hs[method][15,3,:],lw=2.5,zorder=m,label=method)
+end
+plot(xs,true_pdf,"k",lw=2.5, label="true pdf")
+xlabel("x"); ylabel("f(x)")
+legend()
+title(L"w = 15.0, $10^5$ iterations")
+
+subplot(1,2,2)
+for (m,method) in enumerate(keys(err))
+    plot(xs,hs[method][15,5,:],lw=2.5,zorder=m,label=method)
+end
+plot(xs,true_pdf,"k",lw=2.5, label="true pdf")
+xlabel("x")
+title(L"w = 15.0, $10^6$ iterations")
+savefig("pdf_comparison.jpeg")
---
+++ examples/standard_distr.jl
@@ -0,0 +1,43 @@
+using SliceSamplingProject, PyPlot, Distributions
+close("all") # close all open figures
+rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams")
+rcParams["font.size"] = 28
+
+fcts = Dict()
+
+# Gaussian
+σ1   = 2.0
+fcts["Normal(3,2)"] = x -> pdf(Normal(3, 2), x)
+
+# exponential
+fcts["Exponential(1)"] = x -> pdf(Exponential(1.0), x)
+
+fcts["Uniform(0,1)"] = x -> pdf(Uniform(0,1), x)
+
+# Beta
+fcts["Beta(3,10)"] = x -> pdf(Beta(3,10), x)
+
+n  = length(fcts)
+x0 = 0.5
+figure(figsize=(28,9))
+for (d, dist) in enumerate(keys(fcts))
+    # stepping out procedure
+    subplot(2,n,d)
+    x = single_var(fcts[dist],x0;iters=10^5,w=1.0,method="step_out")
+    hist(x,bins=40,color="grey",alpha=0.4,density=true,label="stepping out")
+    xstart, xend = extrema(x)
+    plot(xstart:0.01:xend,fcts[dist].(xstart:0.01:xend),color="black",lw=2,label="true pdf")
+    title(dist)
+    if d == 1 ylabel("f(x)") end
+    if d == 2 legend() end
+    # doubling
+    subplot(2,n,d+n)
+    x = single_var(fcts[dist],x0;iters=10^5,w=1.0,method="doubling")
+    hist(x,bins=40,color="CornFlowerBlue",alpha=0.4,density=true,label="doubling")
+    xstart, xend = extrema(x)
+    plot(xstart:0.01:xend,fcts[dist].(xstart:0.01:xend),color="black",lw=2,label="true pdf")
+    xlabel("x")
+    if d == 1 ylabel("f(x)") end
+    if d == 2 legend() end
+end
+savefig("simple_dists.jpeg")
